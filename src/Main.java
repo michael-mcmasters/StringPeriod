61c871946bc700c5ebd96bac6a6034c2ceca0b2b
michael-mcmasters <jmcmasters411@gmail.com>
2020-11-11 17:22:36 -0500

    Commented code

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,15 @@ public class Main {
         // -1
         System.out.println(stringPeriods("abaxbaba"));
 
+        // abcab
+        System.out.println(stringPeriods("abcababcababcab"));
     }
 
     private static String stringPeriods(String sen) {
+        // Run loop for half the length of sen, because a substring longer than that is not a pattern.
         for (int i = 1; i < sen.length() / 2; i++) {
             String subString = sen.substring(0, i);
-            boolean foundPattern = checkForMatchingSubstrings(sen, subString);
-            if (foundPattern)
+            if (checkForMatchingSubstrings(sen, subString))
                 return subString;
         }
 
@@ -29,17 +31,22 @@ public class Main {
     }
 
     private static boolean checkForMatchingSubstrings(String sen, String subString) {
+        // length of "ab" must go into length of "ababab" perfectly for it to be a pattern.
+        // (6 / 2 equals 3 with a remainder of 0).
+        // If remainder is 0, it is perfectly divisible and we continue. If not, it is not a pattern and we return false.
         if (sen.length() % subString.length() != 0)
             return false;
 
+        // If subString is "ab" and sen is "ababab"
+        // This will compare "ab" to substring(0, 2) ... substring(2, 4) ... substring(4, 6)
         for (int i = 0; i < sen.length(); i += subString.length()) {
             String subStringToCompare = sen.substring(i, i + subString.length());
             if (!subString.equals(subStringToCompare)) {
-                return false;
+                return false;       // If two substrings do not match, return false
             }
         }
 
-        // If subString passed conditions above, it is a pattern.
+        // If all conditions above passed, it is a pattern.
         return true;
     }
 }
