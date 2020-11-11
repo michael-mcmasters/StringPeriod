1d4d7064eaae911bdb524c5b0feb00b3fd71bcc4
michael-mcmasters <jmcmasters411@gmail.com>
2020-11-11 14:46:26 -0500

    Change algorithm

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,38 @@ public class Main {
     // This is a problem from CoderByte called StringPeriods.
     public static void main(String[] args) {
 
-        String result = stringPeriods("ababab");
+        // ab
+        System.out.println(stringPeriods("ababab"));
 
-        System.out.println(result);
+        // -1
+        System.out.println(stringPeriods("abaxbab"));
+
+        // -1
+        System.out.println(stringPeriods("abaxbaba"));
+
     }
 
     private static String stringPeriods(String sen) {
-        String result = "";
-
         String subString = sen.substring(0, 2);
-        if (sen.length() % subString.length() == 0) {
-            int amountToMultiply = sen.length() / subString.length();
-            for (int i = 0; i < amountToMultiply; i++) {
-                result += subString;
-            }
-            return result;
-        }
+        String result = checkForMatchingSubstrings(sen, subString);
+        return result;
+    }
 
+    private static String checkForMatchingSubstrings(String sen, String subString) {
+        if (sen.length() % subString.length() == 0) {
+            boolean patternDetected = true;
+            for (int i = 0; i < sen.length(); i += subString.length()) {
+                if (sen.substring(i, i + 2).equals(subString)) {
+                    continue;
+                }
+                patternDetected = false;
+                break;
+            }
+
+            if (patternDetected == true) {
+                return subString;
+            }
+        }
         return "-1";
     }
 }
